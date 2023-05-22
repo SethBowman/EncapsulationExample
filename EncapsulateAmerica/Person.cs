43bf03c08381ecc7ecf0c35f024adc6d2d6804d1
Seth Bowman <brandonsethbowman@gmail.com>
2023-05-22 16:57:56 -0500

    Example created

---
+++ EncapsulateAmerica/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulateAmerica
+{
+    public class Person
+    {
+        private double _wallet = 0;
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void AddMoney(double moneyToAdd)
+        {
+            _wallet += moneyToAdd;
+        }
+
+        public double CheckWallet()
+        {
+            return _wallet;
+        }
+    }
+}
---
+++ EncapsulateAmerica/Program.cs
@@ -0,0 +1,19 @@
+﻿using EncapsulateAmerica;
+
+var seth = new Person();
+seth.Name = "Seth";
+seth.Age = 29;
+
+seth.AddMoney(500.00);
+
+var amountInWallet = seth.CheckWallet();
+
+Console.WriteLine("Money in wallet:");
+Console.WriteLine(amountInWallet);
+
+Console.WriteLine("Adding money..");
+seth.AddMoney(500.00);
+
+amountInWallet = seth.CheckWallet();
+Console.WriteLine(amountInWallet);
+
